47c6d85dd8cc70e90209d6b0cfc1a7a3c63a74bf
micromarco12 <marco@ciirus.com>
2025-05-10 14:28:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/merge-audio", async (req, res) => {
 
   const { files, outputName, preset = "radio" } = req.body;
 
-  const silenceMs = 3000;
+  const silenceMs = 5000;
   const fadeMs = 150;
   const applyCompression = true;
 
